26e66729f051e72b655dec1b3cdd0691e70292cf
hebergutierrez.p@gmail.com <hebergutierrez.p@gmail.com>
2018-01-22 14:46:50 -0600

    Bee class - solved

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,9 @@
-class Bee {
+class Bee extends Grub {
   // TODO..
+  constructor(age, color, job) {
+    super()
+    this.age = 5;
+    this.color = 'yellow';
+    this.job = 'Keep on growing'
+  }
 };
--- src/Grub.js
+++ src/Grub.js
@@ -2,7 +2,7 @@ class Grub {
   constructor(age, color, food) {
     this.age = 0;
     this.color = 'pink';
-    this.food = 'jelly'
+    this.food = 'jelly';
   }
   eat() {
     return 'Mmmmmmmmm jelly'
